a70b1f249486052e120c78b38ce60ccd53574591
CodPy <47329731+CodPy@users.noreply.github.com>
2019-02-08 16:36:44 +0300

    Update read_main.py

--- read_main.py
+++ read_main.py
@@ -1,49 +1,99 @@
-dom
+mport paramiko
 
-import time
-
-import mysql.connector
-
- 
-
- 
-
- 
 import datetime
 
 import requests
 
 import os
 
-import xml.dom.mini
-# функция преобразования даты в формат SPLUNK
+import xml.dom.minidom
 
-def transform_splunk_date(dt):
+import mysql.connector
 
-    Date_splunk=str (dt.month)+'/'+str (dt.day)+'/'+str (dt.year)+':'+str (dt.hour)+':'+str (dt.minute)+':'+str (dt.second)
+import requests
 
-    return Date_splunk
+from LibEkis import *
+
+ 
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# InsecureRequestWarning отключение
 
  
 
-# функция подготовки запроса к SPLUNK
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-def createsplunkquery (Policy_number):
-
-    EndTime_in = datetime.datetime.now()
-
-    delta = datetime.timedelta(days=500)
-
-    StartTime_in = EndTime_in - delta
-
-    StartTime = transform_splunk_date(StartTime_in)
-
-    EndTime = transform_splunk_date(EndTime_in)
-
-    query="""index=* earliest_time=%s latest_time=%s  [search index=*  %s message.ext.ContractId!=NULL | table "message.ext.ContractId"] "message.Action"=GetOsagoContract "message.Message"=Completed "message.Node"="ufo-be-prd-05" |  spath input=message.ext.Data output=StartDate  path=CalcRequest.StartDate | spath input=message.ext.Data output=EndDate path=CalcRequest.EndDate | spath input=message.ext.Data output=Num path=LoadPolicyToRsaResult.PolicyNumber | spath input=message.ext.Data output=Draft path=LoadPolicyToRsaResult.DraftPolicyId | table StartDate, EndDate ,Num,Draft""" %(StartTime,EndTime,Policy_number)
-
-    print(query)
-
-   # query = '''index = "ufo" earliest_time=%s latest_time=%s "message.Action"=ProjectPolicyServiceClient.loadPolicy  message.Message=Started  [search index = "ufo" %s "message.Action"=LoadPolicy | table message.CorrelationId] | spath input=message.ext.Data output=DraftPolicyID path=ns2:PolicyEOSAGORequest.DraftPolicyRequest.DraftPolicyID | search DraftPolicyID | spath input=message.ext.Data  output=DateActionBeg path=ns2:PolicyEOSAGORequest.DraftPolicyRequest.Da
+res = ''
 
  
+
+mysqlconnect = connectmysql()
+
+curs=mysqlconnect.cursor()
+
+sql='SELECT * FROM XXX_contract1 xc WHERE xc.state_=4 '
+
+curs.execute(sql)
+
+row=curs.fetchone()
+
+print ("запросиз БД ",row)
+
+print (row)
+
+ 
+
+while row is not None:
+
+    mysqlconnect_1 = connectmysql()
+
+    print (row[1])
+
+    res = splunkresult(row[1])
+
+    if  res is not None:
+
+      curs1 = mysqlconnect_1.cursor()
+
+      slq_1 = ''' UPDATE XXX_contract1 set  BEGIN_DATE = %r, END_DATE=%r, State_=%r, SITE_ID_CONTRACT_NUMBER= %r WHERE  POLICY_NO=%r''' %(res[0],res[1],2,res[3],row[1])
+
+      print('непустая ветка')
+
+      print(slq_1)
+
+      curs1.execute(slq_1)
+
+      mysqlconnect_1.commit()
+
+ 
+
+ 
+
+    elif res is None:
+
+        curs1 = mysqlconnect_1.cursor()
+
+        #slq_1 = ''' UPDATE XXX_contract set Policy_ID = %r, Date_Beg = %r, State_=  %r WHERE policy_Number=%r''' % ('', '', 3, row[1])
+
+        slq_1 = ''' UPDATE XXX_contract1 set   BEGIN_DATE = %r, END_DATE=%r, State_= %r WHERE  POLICY_NO = %r ''' % ('', '',  3, row[1])
+
+        print('пустая ветка')
+
+        print(slq_1)
+
+        curs1.execute(slq_1)
+
+        mysqlconnect_1.commit()
+
+ 
+
+    row = curs.fetchone()
+
+ 
+
+ 
+
+ 
+
+print('Обработка завершена')
